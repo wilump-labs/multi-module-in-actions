ee9aa75420db3d0dde238b3b1ac96892c2b17667
wilump(Heedong Kim) <wilump@dunamu.com>
2023-12-14 21:13:03 +0900

    fix: core-api에 monitoring, db-core 의존성 추가

--- core/core-api/build.gradle.kts
+++ core/core-api/build.gradle.kts
@@ -8,6 +8,8 @@ tasks.getByName("jar") {
 
 dependencies {
     implementation(project(":support:logging"))
+    implementation(project(":support:monitoring"))
+    implementation(project(":storage:db-core"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
